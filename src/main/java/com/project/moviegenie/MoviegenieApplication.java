f337aa744c1125b5b16c2b1cd640a8d2acb21aee
tlqkrus012345 <tlqkrus012345@naver.com>
2023-07-31 22:46:38 +0900

    [#1] 프로젝트 생성

---
+++ src/main/java/com/project/moviegenie/MoviegenieApplication.java
@@ -0,0 +1,13 @@
+package com.project.moviegenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviegenieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviegenieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/moviegenie/MoviegenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.moviegenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviegenieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
