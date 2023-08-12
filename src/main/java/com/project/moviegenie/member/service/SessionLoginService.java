8c40489c7c660a58e1e5db1ae143f9d8a55ae43b
tlqkrus012345 <tlqkrus012345@naver.com>
2023-08-12 21:01:32 +0900

    [#8] 세션 로그인 서비스 클래스 작성

---
+++ src/main/java/com/project/moviegenie/member/service/SessionLoginService.java
@@ -0,0 +1,23 @@
+package com.project.moviegenie.member.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+@Service
+@RequiredArgsConstructor
+public class SessionLoginService implements LoginService{
+
+    private final HttpSession httpSession;
+    private static final String MEMBER_ID = "member_id";
+    @Override
+    public void login(Long id) {
+        httpSession.setAttribute(MEMBER_ID, id);
+    }
+
+    @Override
+    public void logout() {
+        httpSession.removeAttribute(MEMBER_ID);
+    }
+}
