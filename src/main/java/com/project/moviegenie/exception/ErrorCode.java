5e47db2b63126a1da062f64ca957435272e4b3ec
tlqkrus012345 <tlqkrus012345@naver.com>
2023-08-12 21:02:57 +0900

    [#8] 이메일과 패스워드 불일치 에러코드 작성

--- src/main/java/com/project/moviegenie/exception/ErrorCode.java
+++ src/main/java/com/project/moviegenie/exception/ErrorCode.java
@@ -10,7 +10,9 @@ public enum ErrorCode {
 
     DUPLICATED_EMAIL(HttpStatus.CONFLICT, "중복된 이메일입니다."),
     DUPLICATED_NICKNAME(HttpStatus.CONFLICT, "중복된 닉네임입니다."),
-    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "유효성 검사에 실패했습니다.");
+    VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "유효성 검사에 실패했습니다."),
+    NOT_FOUND_EMAIL(HttpStatus.NOT_FOUND, "이메일을 찾을 수 없습니다."),
+    NOT_FOUND_PASSWORD(HttpStatus.NOT_FOUND, "패스워드를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
