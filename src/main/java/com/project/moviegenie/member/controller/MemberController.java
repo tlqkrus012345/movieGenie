04d377308f8d166bf02fd44a13a9c24eb3082ef3
tlqkrus012345 <tlqkrus012345@naver.com>
2023-08-12 21:03:11 +0900

    [#8] 로그인 로그아웃 컨트롤러 작성

--- src/main/java/com/project/moviegenie/member/controller/MemberController.java
+++ src/main/java/com/project/moviegenie/member/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.project.moviegenie.member.controller;
 
+import com.project.moviegenie.member.dto.MemberLoginRequest;
+import com.project.moviegenie.member.dto.MemberLoginResponse;
 import com.project.moviegenie.member.dto.MemberSignUpRequest;
 import com.project.moviegenie.member.domain.Member;
 import com.project.moviegenie.member.dto.MemberSignUpResponse;
+import com.project.moviegenie.member.service.LoginService;
 import com.project.moviegenie.member.service.MemberService;
 import com.project.moviegenie.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -17,12 +20,29 @@ import javax.validation.Valid;
 public class MemberController {
 
     private final MemberService memberService;
+    private final LoginService loginService;
     private final PasswordEncoder passwordEncoder;
     @PostMapping("/sign-up")
     public ApiResponse<MemberSignUpResponse> signUp(@RequestBody @Valid MemberSignUpRequest dto) {
-
         Member requestMember = MemberSignUpRequest.toEntity(dto, passwordEncoder);
 
         return ApiResponse.success(MemberSignUpResponse.toDto(memberService.signUp(requestMember)));
+    }
+
+    @PostMapping("login")
+    public ApiResponse<MemberLoginResponse> login(@RequestBody @Valid MemberLoginRequest dto) {
+        memberService.isValidMember(MemberLoginRequest.toEntity(dto), passwordEncoder);
+
+        Member loginMember = memberService.findMemberByEmail(dto.getEmail());
+        loginService.login(loginMember.getId());
+
+        return ApiResponse.success(MemberLoginResponse.toDto(loginMember.getNickName()));
+    }
+
+    @GetMapping("logout")
+    public ApiResponse<?> logout() {
+        loginService.logout();
+
+        return ApiResponse.success();
     }
 }
