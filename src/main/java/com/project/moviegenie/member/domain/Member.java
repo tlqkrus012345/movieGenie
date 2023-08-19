0ccfab6918af4fe0cd03e4785f44c4e3ed0211d5
tlqkrus012345 <tlqkrus012345@naver.com>
2023-08-19 20:15:36 +0900

    [#8] OAuth2로 가입한 회원을 위한 칼럼 추가

--- src/main/java/com/project/moviegenie/member/domain/Member.java
+++ src/main/java/com/project/moviegenie/member/domain/Member.java
@@ -6,12 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Table(name = "members")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Member {
+public class Member implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,10 +23,28 @@ public class Member {
     private String password;
     @Column(nullable = false)
     private String nickName;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MemberRole memberRole;
+
+    private String provider;
+    private String providerId;
+
     @Builder
-    public Member(String email, String password, String nickName) {
+    public Member(String email, String password, String nickName, MemberRole memberRole, String provider, String providerId) {
         this.email = email;
         this.password = password;
+        this.nickName = nickName;
+        this.memberRole = memberRole;
+        this.provider = provider;
+        this.providerId = providerId;
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
+
+    public void updateNickName(String nickName) {
         this.nickName = nickName;
     }
 }
