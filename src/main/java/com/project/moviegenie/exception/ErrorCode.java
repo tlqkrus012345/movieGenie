c5738621230cccab1e44b6bfc24368e1b171155c
tlqkrus012345 <tlqkrus012345@naver.com>
2023-09-15 16:52:04 +0900

    [#17] 없는 장르 에러 코드 추가

--- src/main/java/com/project/moviegenie/exception/ErrorCode.java
+++ src/main/java/com/project/moviegenie/exception/ErrorCode.java
@@ -13,6 +13,7 @@ public enum ErrorCode {
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "유효성 검사에 실패했습니다."),
     NOT_FOUND_EMAIL(HttpStatus.NOT_FOUND, "이메일을 찾을 수 없습니다."),
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다"),
+    NOT_FOUND_GENRE(HttpStatus.NOT_FOUND, "해당 장르는 없는 장르입니다."),
     NOT_FOUND_PASSWORD(HttpStatus.NOT_FOUND, "패스워드를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
