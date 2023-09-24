c264dfa6f12dd21ced6bba1180a7e8a9bb7c177b
tlqkrus012345 <tlqkrus012345@naver.com>
2023-09-24 19:57:34 +0900

    [#17] MovieReviewResponse에 최초 등록된 날짜 (yyyy/MM/dd) 컬럼 추가

--- src/main/java/com/project/moviegenie/review/dto/MovieReviewResponse.java
+++ src/main/java/com/project/moviegenie/review/dto/MovieReviewResponse.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.format.DateTimeFormatter;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -16,8 +18,17 @@ public class MovieReviewResponse {
     private String context;
     private String genre;
     private String writer;
+    private String registerDate;
 
     public static MovieReviewResponse toDto(Review review) {
-        return new MovieReviewResponse(review.getTitle(), review.getContext(), review.getGenre().name(), review.getWriter().getNickName());
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        String formattedDate = review.getCreatedDate().format(dateTimeFormatter);
+
+        return new MovieReviewResponse(
+                review.getTitle(),
+                review.getContext(),
+                review.getGenre().name(),
+                review.getWriter().getNickName(),
+                formattedDate);
     }
 }
